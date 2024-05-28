3ca6129308f3eddda45f8071cb52ccf2a419c498
inkjesse <168197140+inkjesse@users.noreply.github.com>
2024-05-28 13:04:25 +0200

    fix

--- components/AddNetworkScript.tsx
+++ components/AddNetworkScript.tsx
@@ -6,7 +6,7 @@ const networkParams = {
     symbol: 'ETH',
     decimals: 18,
   },
-  rpcUrls: ['https://rpc.op-one-testnet.gelato.digital'],
+  rpcUrls: ['https://rpc.op-one-testnet.gelato.digital?'], //@TODO WEIRDEST BUG EVER. IF YOU CHANGE THIS `digital?` into `digital` it breaks. You can change it into EVERYTHING except `digital`
   blockExplorerUrls: ['https://explorer.op-one-testnet.gelato.digital/'],
 };
 
