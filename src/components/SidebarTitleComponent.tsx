39fe96f65806ec04c0c3889820b35d30d6f765c0
dkrabdev <171172447+dkrabdev@users.noreply.github.com>
2024-06-10 12:07:26 +0200

    chore: applied required style changes

--- src/components/SidebarTitleComponent.tsx
+++ src/components/SidebarTitleComponent.tsx
@@ -16,15 +16,19 @@ export const SidebarTitleComponent: React.FC<SidebarTitleComponentProps> = ({
   const isActive = route === asPath;
 
   if (type === "separator") {
-    return <div className="font-bold text-black dark:text-white">{title}</div>;
+    return (
+      <div className="font-bold text-black dark:text-magic-white">{title}</div>
+    );
   }
 
   return (
     <div
       className={clsx(
-        "ink-sidebar-item px-2 py-1.5  rounded-sm w-full hover:bg-magic-purple/20 transition-all",
+        "ink-sidebar-item px-2 py-1.5 rounded-sm w-full transition-all",
         {
-          "bg-magic-purple/20": isActive,
+          "text-ink-grey-400 dark:text-magic-white dark:hover:text-ink-grey-700 hover:text-ink-grey-700 hover:bg-ink-grey-100":
+            !isActive,
+          "bg-magic-semi-deep-purple/15 text-magic-purple": isActive,
         }
       )}
     >
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,21 @@ module.exports = {
       colors: {
         magic: {
           purple: "#7132F5",
+          'semi-deep-purple': '#855BFB',
           'deep-purple': "#2B1463",
           black: "#101114",
           white: "#F0EFFF",
           'soft-pink': "#F7D2FE",
-        }
-      }
+        },
+        'ink-grey': {
+          100: '#F3F4F6',
+          400: '#6B7280',
+          700: '#374151',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
     },
   },
   plugins: [],
