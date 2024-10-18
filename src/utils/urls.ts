d40d17419adecd6787b6d80ffd50db50ebeaa214
Cruz Molina <7537712+CruzMolina@users.noreply.github.com>
2024-10-18 10:22:59 -0700

    docs: update testnet rpc & related info

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -2,10 +2,10 @@ export const URLS = {
   githubOrgUrl: "https://github.com/inkonchain",
   statusPageUrl: "https://status.optimism.io/",
   faucetUrl: "https://app.optimism.io/faucet",
-  inkubatorUrl: "https://inkchain.xyz/inkubator",
+  inkubatorUrl: "https://inkonchain.com/inkubator",
   discordUrl: "https://discord.com/invite/inkchain",
   repositoryUrl: "https://github.com/inkonchain/docs",
-  developerWailistUrl: "https://inkchain.xyz",
-  feedbackUrl: "https://inkchain.xyz",
+  developerWailistUrl: "https://inkonchain.com",
+  feedbackUrl: "https://inkonchain.com",
   editDocsOnGithub: "https://github.com",
 };
