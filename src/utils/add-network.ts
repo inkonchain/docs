4e8e84fe9feb586e125da0aab8ece5666e98d81f
Cruz Molina <cruz.adam.molina@gmail.com>
2024-10-18 10:27:35 -0700

    docs: update testnet rpc & related info (#110)

--- src/utils/add-network.ts
+++ src/utils/add-network.ts
@@ -1,13 +1,13 @@
 const networkParams = {
-  chainId: "0xDEA27", // 911911 in hexadecimal
+  chainId: "0xBA5ED", // 763373 in hexadecimal
   chainName: "Ink Sepolia",
   nativeCurrency: {
     name: "Ether",
     symbol: "ETH",
     decimals: 18,
   },
-  rpcUrls: ["https://sepolia-rpc.inkchain.xyz"],
-  blockExplorerUrls: ["https://sepolia-explorer.inkchain.xyz/"],
+  rpcUrls: ["https://rpc-gel-sepolia.inkonchain.com"],
+  blockExplorerUrls: ["https://explorer-sepolia.inkonchain.com/"],
 };
 
 export async function addNetwork() {
