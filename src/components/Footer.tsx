20943885800d225d277e336c67f8e193c6747a53
JOHUBKRA <184514753+johubkra@users.noreply.github.com>
2024-10-29 22:04:29 +0530

    change heart color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <div className="flex flex-col items-center justify-center gap-1 py-16 border-t selection:bg-magic-white">
       <div className="px-6 mx-auto max-w-[90rem] w-full flex justify-between gap-2">
         <div className="text-sm text-gray-600">
-          Made with ❤️ by the Inkonchain team
+          Made with 💜 by the Inkonchain team
         </div>
 
         <ThemeToggle />
